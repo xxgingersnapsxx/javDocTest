44f10ccc74bd953f241c4ae1b91d812ca407383e
xxgingersnapsxx <zxgingersnapsxz@gmail.com>
2021-06-18 14:50:28 +0900

    innitial commit

---
+++ src/kr/or/ddit/basic/JavaDocTest.java
@@ -0,0 +1,23 @@
+package kr.or.ddit.basic;
+// Java Doc 파일 만들기 예제
+
+/**
+ * 
+ * @author 홍길동
+ * @version 1.0
+ * <p>
+ * 	파일명 : JavaDocTest.java<br>
+ * 	설명 : JavaDoc문서 작성을 위한 연습용 Interface<br><br>
+ * 
+ *  수정 이력 <br>
+ *  -----------------------------------------<br>
+ *  수정일자 : 2021-06-18<br>
+ *  작 성 자 : 홍길동<br>
+ *  수정 내용 : 최초 생성<br>
+ *  -----------------------------------------<br>
+ * </p>
+ *
+ */
+public interface JavaDocTest {
+
+}
